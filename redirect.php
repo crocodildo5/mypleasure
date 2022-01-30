84a7ffdb4bf55d823c1f82bc1acda45d94ae61ed
crocodildo5 <94800874+crocodildo5@users.noreply.github.com>
2022-01-30 15:12:59 +0300

    Add files via upload

---
+++ redirect.php
@@ -0,0 +1,30 @@
+<?php
+/*редирект на заранее заготовленный html файл
+echo '
+    <meta http-equiv="Refresh"
+    content="0 ; URL=head.html">
+';
+exit();*/
+
+/*редирект через header
+header("Location: http://www.youtube.com");
+exit();*/
+
+/*внешний редирект
+header('Location:head.html');
+exit();*/
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Первая страница</h1>
+
+</body>
+</html>
